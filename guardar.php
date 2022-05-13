d1fbea25cd848bf4a7cb814ef840f8ae9904a032
Tu Nombre <mario@fca.unam.mx>
2022-05-12 19:57:56 -0500

    validaciones en cliente y server

--- guardar.php
+++ guardar.php
@@ -1,20 +1,35 @@
 <?php
 	include 'conexion.php';
-	$nombre = $_POST['nombre'];
-	$apaterno = $_POST['apaterno'];
-	$amaterno = $_POST['amaterno'];
-	$ciudad = $_POST['ciudad'];
+	$nombre = strip_tags($_POST['nombre']);
+	$apaterno = strip_tags($_POST['apaterno']);
+	$amaterno = strip_tags($_POST['amaterno']);
+	$ciudad = strip_tags($_POST['ciudad']);
+
+	if(preg_match('/[A-Za-z áéíóúñ]{2,50}/i', $nombre)){
+		if(preg_match('/[A-Za-z áéíóúñ]{2,50}/i', $apaterno)){
+	        	if(preg_match('/[A-Za-z áéíóúñ]{2,50}/i', $amaterno)){
+                        	if(preg_match('/[A-Za-z áéíóúñ]{2,50}/i', $ciudad)){
+                        		$insercion= "insert into usuario(nombre,apaterno,amaterno,ciudad) values('$nombre','$apaterno','$amaterno','$ciudad')";
+        				$query = pg_query($con,$insercion);
+        				if($query){
+                				echo "Se guardo en la base de datos";
+                				echo "Un gusto ".$nombre." ".$apaterno." ".$amaterno." de ".$ciudad;
+        				} else {
+                				echo "Hubo un error";
+        				}
+
+        				pg_close($con);
 	
-	$insercion= "insert into usuario(nombre,apaterno,amaterno,ciudad) values('$nombre','$apaterno','$amaterno','$ciudad')";
-	$query = pg_query($con,$insercion);
-	if($query){
-		echo "Se guardo en la base de datos";
-		echo "Un gusto ".$nombre." ".$apaterno." ".$amaterno." de ".$ciudad;
-	} else {
-		echo "Hubo un error";
+                		}
+	                }
+        	}
+
 	}
-	
-	pg_close($con);
+	else {
+		echo "Los datos no cumplen las condiciones";
+
+	}	
+
 
 ?>
 
--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-	$("#enviar").click(function(){
+	$('#cuestionario').validate({
+
+		submitHandler: function(){
 		$.post("guardar.php",
 			{
 				nombre: $("#nombre").val(),
@@ -11,5 +13,51 @@ $(document).ready(function(){
 				alert("Datos: "+ data+"Status:"+status);
 				$("#cuestionario").trigger("reset");
 			});
+	}, 
+		rules: {
+			nombre: {
+				required: true,
+				minlength: 2,
+				maxlength: 50
+			},
+			apaterno: {
+                                required: true,
+                                minlength: 2,
+                                maxlength: 50
+                        },
+			amaterno: {
+                                required: true,
+                                minlength: 2,
+                                maxlength: 50
+                        },
+			ciudad: {
+                                required: true,
+                                minlength: 2,
+                                maxlength: 50
+                        }
+		},
+		messagges: {
+			nombre: {
+				required: "Campo obligatorio, favor de ingresar su nombre",
+				minlenght: "El nombre debe ser por lo menos de 2 letras",
+				maxlenght: "El nombre tiene como maximo 50 caracteres"
+			},
+			apaterno: {
+                                required: "Campo obligatorio, favor de ingresar su apellido paterno",
+                                minlenght: "El apellido paterno debe ser por lo menos de 2 letras",
+                                maxlenght: "El apellido paterno tiene como maximo 50 caracteres"
+                        },
+			amaterno: {
+                                required: "Campo obligatorio, favor de ingresar su apellido materno",
+                                minlenght: "El apellido materno debe ser por lo menos de 2 letras",
+                                maxlenght: "El apellido materno tiene como maximo 50 caracteres"
+                        },
+			ciudad: {
+                                required: "Campo obligatorio, favor de ingresar su ciudad de origen",
+                                minlenght: "La ciudad debe ser por lo menos de 2 letras",
+                                maxlenght: "La ciudad tiene como maximo 50 caracteres"
+                        }
+		}
 	});
+
 });
